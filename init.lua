8fc2468684a3ae476d9bd07e99200ea71a40abeb
Karim Salakhov <kisalakhov@edu.hse.ru>
2026-09-19 22:13:00 +0300

    i did some cool shi4

--- init.lua
+++ init.lua
@@ -204,9 +204,6 @@ end,
     [wide] = resolutions.wide,
 
     [toggle_ninbot] = function()
-    [wide] = resolutions.wide,
-
-    [toggle_ninbot] = function()
         if not is_ninb_running() then
             waywall.exec("java -Dawt.useSystemAAFontSettings=on -jar " .. nb_path)
             waywall.show_floating(true)
