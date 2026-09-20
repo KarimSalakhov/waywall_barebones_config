ee6b2e687f6c5078468aaa123f39f8971c41b0f9
Karim Salakhov <kisalakhov@edu.hse.ru>
2026-09-20 12:20:10 +0300

    best setup

--- init.lua
+++ init.lua
@@ -177,23 +177,6 @@ local config = {
     },
 }
 
-config.actions = {
-    ["F4"] = function()
-    remaps_enabled = not remaps_enabled
-
-    if remaps_enabled then
-        waywall.set_remaps(remapped_kb)
-    else
-        waywall.set_remaps({})
-    end
-end,
-    [thin] = function()
-    if waywall.get_key("F3") then
-        return false
-    end
-    resolutions.thin()
-end,
-
 [tall] = function()
     if waywall.get_key("F3") then
         return false
@@ -211,11 +194,13 @@ end,
         end
     end,
     ["*-C"] = function()
-        if waywall.get_key("F3") then
-            waywall.show_floating(true)
-        end
+    if waywall.get_key("F3") then
+        waywall.show_floating(true)
         return false
-    end,
+    else
+        return false
+    end
+end,
     [launch_paceman] = function()
         if not is_pacem_running() then
             waywall.exec("java -jar " .. pacem_path .. " --nogui")
